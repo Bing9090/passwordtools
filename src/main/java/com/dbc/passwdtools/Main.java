e524f3a7873290e842a43a5024162a85c0a0eb9e
dxbaicai <ye56@vip.qq.com>
2020-09-29 17:32:13 +0800

    add salt param

--- src/main/java/com/dbc/passwdtools/Main.java
+++ src/main/java/com/dbc/passwdtools/Main.java
@@ -2,15 +2,24 @@ package com.dbc.passwdtools;
 
 import org.jasypt.util.text.BasicTextEncryptor;
 
+/**
+ * @author dabaicai
+ * 2020-09-29
+ * useage: java -jar passwdtools.jar "{salt}" "{passwd}"
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-        //String passwd = "159*Ospdb";
+        if (args.length != 2) {
+            System.out.println("Args error! The useage: java -jar passwdtools.jar \"{salt}\" \"{passwd}\"");
+            return;
+        }
         BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
         //加密所需的salt(盐)
-        textEncryptor.setPassword("arcs");
+        textEncryptor.setPassword(args[0]);
         //要加密的密码
-        String password = textEncryptor.encrypt(args[0]);
+        String password = textEncryptor.encrypt(args[1]);
         System.out.println("en password: " + password);
 
         //String dep = textEncryptor.decrypt("smvQf6jiYe7sbvhtA3SkvqhGb8s3RfRV");
